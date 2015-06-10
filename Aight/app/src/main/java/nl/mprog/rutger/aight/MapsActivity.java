fe7e7902c0e749f89fdcdb40453b1a65906bd666
Rutger van de Lagemaat <zabilos@gmail.com>
2015-06-10 15:14:13 +0200

    current location is now automatically stored on parse and all stored locations are retrieved and logged

--- Aight/app/src/main/java/nl/mprog/rutger/aight/MapsActivity.java
+++ Aight/app/src/main/java/nl/mprog/rutger/aight/MapsActivity.java
@@ -1,14 +1,26 @@
 package nl.mprog.rutger.aight;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.FindCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -24,7 +36,14 @@ public class MapsActivity extends FragmentActivity {
 
         Parse.initialize(this, "41OkLo6j1hdKZsx1n1iGfvFtwRALWLerZ45glOZ8", "zXSgVFnOxCpRktMpvdTjGQ5YKObO69qqj9bFdNNm");
         setUpMapIfNeeded();
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
+
+
+
 
     @Override
     protected void onResume() {
@@ -56,7 +75,6 @@ public class MapsActivity extends FragmentActivity {
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap();
-                mMap.setMyLocationEnabled(true);
             }
         }
     }
@@ -68,9 +86,62 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(52.354892, 4.955294)).title("Science"));
+
+        // the following lines are based on this tutorial:
+        // http://bit.ly/1KoNLoA
+
+        // Enable location in map
+        mMap.setMyLocationEnabled(true);
+
+        // Get LocationManager object from System Service LOCATION_SERVICE
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        // Create a criteria object to retrieve provider
+        Criteria criteria = new Criteria();
+
+        // Get the name of the best provider
+        String provider = lm.getBestProvider(criteria, true);
+
+        // Get Current Location
+        Location myLocation = lm.getLastKnownLocation(provider);
+
+
+        // Get latitude of the current location
+        double latitude = myLocation.getLatitude();
+
+        // Get longitude of the current location
+        double longitude = myLocation.getLongitude();
+
+        // Create a LatLng object for the current location
+        LatLng UserLocation = new LatLng(latitude, longitude);
+        String UserLocationString = String.valueOf(UserLocation.toString());
+
+        // Show the current location in Google Map
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(UserLocation));
+
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(52.354892, 4.955294)).title("Science").snippet("Are you sciencing as hard as you can?"));
         mMap.addMarker(new MarkerOptions().position(new LatLng(52.358892, 4.950)).title("park"));
 
-        ParseGeoPoint point = new ParseGeoPoint(40.0, -30.0);
+        ParseGeoPoint point = new ParseGeoPoint(latitude, longitude);
+        ParseObject placeObject = new ParseObject("Location");
+        placeObject.put("location", point);
+        placeObject.put("User", "henk");
+        placeObject.saveInBackground();
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Location");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> list, ParseException e) {
+                //Log.d("locations", list.toString());
+                for (int i = 0; i < list.size(); i++) {
+                    Log.d("Locations", list.get(i).getObjectId());
+                    Log.d("geo", list.get(i).getParseGeoPoint("location").toString());
+                }
+            }
+        });
+
+        
     }
 }
